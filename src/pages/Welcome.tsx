f5246431b4ed216c502cfdff250ff1d07073e3eb
André Ferrari Picolli <picolli@outlook.com.br>
2021-04-20 21:22:28 -0300

    Ajustes tela inicial
    Layoutagem e alterações de fontes

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,59 +1,84 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
+import React from 'react';
+import { 
+    SafeAreaView, 
+    Text, 
+    Image, 
+    TouchableOpacity,
+     StyleSheet,
+     Dimensions,
+     View
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import {Button} from '../components/Button';
+import fonts from '../styles/fonts';
+
 
 export function Welcome(){
-    const[visible, setVisible] = useState(false);
-
-    function handleVisibility(){
-        setVisible(true);
-
-    }
-
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'} 
-                de forma fácil
-            </Text>
-           
-            <Image source={wateringImg} style={styles.image} />            
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'} 
+                    forma fácil
+                </Text>
             
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. 
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text> 
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image} 
+                    resizeMode='contain'
+                />            
+                
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. 
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text> 
 
-            <Button title=">"/>
-            
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                >     
+                <Feather 
+                    name='chevron-right' 
+                    style={styles.buttonIcon}                   
+                />               
+                    
+                </TouchableOpacity>
+            </View>
 
-            
         </SafeAreaView>
     )
 }
-
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+                
+    },
+    wrapper:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'        
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
-        fontWeight: 'bold',
+        fontSize: 28,        
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34,
+        paddingTop: 50
     },
     subtitle:{
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
+    },    
+    image:{        
+        height: Dimensions.get('window').width * 0.7
     },
     button:{
         backgroundColor: colors.green,
@@ -62,15 +87,10 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
-        paddingHorizontal: 10
+        width: 56
     },
-    image:{
-        width: 292,
-        height: 284
-    },
-    buttonText:{
+    buttonIcon:{
         color: colors.white,
-        fontSize: 24
+        fontSize: 32
     }
-
 });
